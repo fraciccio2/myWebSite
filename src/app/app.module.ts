8f1a06bcffbadb4540f7ec99ad27d17973e5f24f
fraciccio2 <francesco.giacomazzo2@gmail.com>
2022-06-15 22:59:58 +0200

    aggiunti portfolio e services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
 import { ExperienceComponent } from './components/experience/experience.component';
+import { ServicesComponent } from './components/services/services.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutComponent,
-    ExperienceComponent
+    ExperienceComponent,
+    ServicesComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule
