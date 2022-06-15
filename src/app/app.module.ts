6165480afdf876c339c14a3bc6035638ccfde9b6
fraciccio2 <francesco.giacomazzo2@gmail.com>
2022-06-15 20:18:53 +0200

    aggiunta sezione esperienze

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule
